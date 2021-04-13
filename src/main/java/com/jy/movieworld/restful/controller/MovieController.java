e9f3b32e8183d5852ccd60648e4f0464855234f0
Your Name <you@example.com>
2021-04-13 23:00:50 +0900

    2021-04-13

--- src/main/java/com/jy/movieworld/restful/controller/MovieController.java
+++ src/main/java/com/jy/movieworld/restful/controller/MovieController.java
@@ -2,13 +2,23 @@ package com.jy.movieworld.restful.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+//@RestController //RestController를 사용하면 반확 객체 사용시 @ResponseBody를 생략하여 사용해도 된다.
 public class MovieController {
+	//@RequestMapping("/main", method=@RequestMethod.GET)
+	//@GetMapping(value="/main")
 	@GetMapping("/main")
-	public String hello() {
-		System.out.println("실행!");
-		
+	public String movieMain() {
 		return "main";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/valueTest")
+	public String valueTest() {
+		String value = "메인 컨트롤러 테스트";
+		return value;
 	}
 }
--- src/main/webapp/WEB-INF/views/main.jsp
+++ src/main/webapp/WEB-INF/views/main.jsp
@@ -3,10 +3,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Movie World</title>
+	<meta charset="UTF-8">
+	<title>Movie World</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
+	<script> 
+		$.ajax({ 
+			type: "GET", 
+			url: "/valueTest", 
+			success: (data) => { 
+				console.log(data); 
+				$('#contents').html(data); 
+			} 
+		}); 
+	</script>
 </head>
 <body>
-	<h2>Movie</h2>
+	<h1>Movie</h1>
+	<div id="contents">
+	</div>
 </body>
 </html>
